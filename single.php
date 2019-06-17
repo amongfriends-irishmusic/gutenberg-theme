744a1ac83fa1626d68a4158e8998bd4d019823c2
Arne Johannessen <git@arne.johannessen.de>
2019-06-18 00:41:59 +0200

    Use special labels for next/prev post links

--- single.php
+++ single.php
@@ -33,14 +33,29 @@ get_header();
 					);
 				} elseif ( is_singular( 'post' ) ) {
 					// Previous/next post navigation.
+					
+					# Use special labels for post links in 'performances' category
+					$af_next_post_label = __( 'Next Post', 'twentynineteen' );
+					$af_prev_post_label = __( 'Previous Post', 'twentynineteen' );
+					$af_is_performance = FALSE;
+					foreach (get_the_category() as $category) {
+						$af_is_performance = $category->slug == 'performances';
+						if ($af_is_performance) { break; }
+					}
+					if ($af_is_performance) {
+						$af_next_post_label = "Nächster Auftritt";
+						$af_prev_post_label = "Vorheriger Auftritt";
+					}
+					
 					the_post_navigation(
 						array(
-							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . $af_next_post_label . '</span> ' .
+								'<span class="screen-reader-text">' . "$af_next_post_label:" . '</span> <br/>' .
 								'<span class="post-title">%title</span>',
-							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
-								'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . $af_prev_post_label . '</span> ' .
+								'<span class="screen-reader-text">' . "$af_prev_post_label:" . '</span> <br/>' .
 								'<span class="post-title">%title</span>',
+							'in_same_term' => TRUE,
 						)
 					);
 				}
